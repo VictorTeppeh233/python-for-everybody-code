076ab75486694c76cd35026d9b14faf9da70b96d
Victor Teppeh <victorteppeh233@gmail.com>
2025-11-30 16:35:16 +0000

    chapter 2

---
+++ 001-welcome-message.py
@@ -0,0 +1,4 @@
+#This program asks the user for their name then prints it
+person = input('Who are you? ')
+
+print('welcome' , person)
---
+++ 002-convert-elevator-floor.py
@@ -0,0 +1,5 @@
+#This program asks user for the floor in Europe then converts it to American
+
+inp = input('Europe floor? ')
+usf = int(inp) + 1
+print("US floor", usf)
